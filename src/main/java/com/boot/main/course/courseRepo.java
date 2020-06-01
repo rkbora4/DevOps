11d08a175045c05f44e7c7f33f3b312627bbc113
Radheshyam Bora <Radheshyam.Bora@external.fcagroup.com>
2020-06-01 13:16:01 +0530

    test

--- src/main/java/com/boot/main/course/courseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.boot.main.course;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-public interface courseRepo extends CrudRepository<Courses, String>{
-	public List<Courses> findByTopicId(String id);
-
-}
